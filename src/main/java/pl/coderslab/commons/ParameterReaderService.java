93b2fe447d0e7b3d3ff13d3028f9ef8ff75a9f4a
Paweł Dąbrowski <fluorix@gmail.com>
2020-04-01 21:59:15 +0200

    LaborController to be tested

--- src/main/java/pl/coderslab/commons/ParameterReaderService.java
+++ src/main/java/pl/coderslab/commons/ParameterReaderService.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 public class ParameterReaderService {
 
@@ -20,11 +21,32 @@ public class ParameterReaderService {
         return id;
     }
 
+    public static double getDoubleFromRequest (HttpServletRequest request, String idName) {
+        double id = 0;
+        String idAsString = request.getParameter(idName);
+        try {
+            id = Double.parseDouble(idAsString);
+        } catch (NumberFormatException | NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("Extraction unsuccessful with id "+ idAsString);
+        }
+        return id;
+    }
+
     public static Timestamp convertLocalDateToTimestamp(LocalDate localDate) {
         return Timestamp.valueOf(LocalDateTime.of(localDate, LocalTime.NOON));
     }
 
     public static LocalDate convertTimestampToLocalDate(Timestamp timestamp) {
         return timestamp.toLocalDateTime().toLocalDate();
+    }
+
+    public static LocalDate parseLocalDate(String s) {
+        try {
+            return LocalDate.parse(s);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            return LocalDate.MIN;
+        }
     }
 }
