01b37cb465456182e5c5eb20418760864caf5c4d
Paweł Dąbrowski <fluorix@gmail.com>
2020-04-11 14:51:17 +0200

    personal form backend validation checked

--- src/main/java/pl/coderslab/person/PersonController.java
+++ src/main/java/pl/coderslab/person/PersonController.java
@@ -7,16 +7,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet (name="PersonController", urlPatterns = "/managePersonDetails")
 public class PersonController extends HttpServlet {
 
+    private static final String PERSON_PATH = "/managePersonDetails";
     private static final String PERSON_FORM = "/WEB-INF/jsp/formPersonDetails.jsp";
     private static final String SHOW_ALL_PERSONS = "/WEB-INF/jsp/allPersonDetails.jsp";
-    private static final String PREP_ALL_PERSONS = "/managePersonDetails?action=view";
+    private static final String PREP_ALL_PERSONS = PERSON_PATH + "?action=view";
     private static final PersonService PERSON_SERVICE = new PersonService();
+    private static final Pattern DATE_FORMAT_PATTERN = Pattern.compile("^([1]9\\d{2}|[2][0|1]\\d{2})-(0[1-9]|1[0-2])-(0[1-9]|1\\d|2\\d|3[0-1])$");
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
@@ -30,9 +35,8 @@ public class PersonController extends HttpServlet {
                 getServletContext().getRequestDispatcher(SHOW_ALL_PERSONS).forward(request, response);
                 return;
             case "edit":
-                PersonDto dto = PERSON_SERVICE.read(id);
-                request.setAttribute("person", dto);
-                request.setAttribute("action", action);
+                setPersonToForm(request, id);
+                System.out.println("GET edit");
                 break;
             case "delete":
                 PERSON_SERVICE.delete(id);
@@ -40,6 +44,13 @@ public class PersonController extends HttpServlet {
                 return;
             default:
         }
+
+        String error = request.getParameter("error");
+        String errorMessage = request.getParameter("errorMessage");
+        request.setAttribute("error", error);
+        request.setAttribute("errorMessage", errorMessage);
+        System.out.println(">>> ERROR: " + error + " MESSAGE: " + errorMessage);
+
         getServletContext().getRequestDispatcher(PERSON_FORM).forward(request, response);
 
 
@@ -60,6 +71,29 @@ public class PersonController extends HttpServlet {
         String birthYear = request.getParameter("birthYear");
         String birthMonth = request.getParameter("birthMonth");
         String birthDay = request.getParameter("birthDay");
+        String fulldate = birthYear + "-" + birthMonth + "-" + birthDay;
+        Matcher matcher = DATE_FORMAT_PATTERN.matcher(fulldate);
+
+        if (firstName == null || firstName.isEmpty()
+                || lastName == null || lastName.isEmpty()
+                || phone == null || phone.isEmpty()
+                || birthYear == null
+                || birthMonth == null
+                || birthDay == null
+                || !matcher.matches()) {
+            // do not validate
+            request.setAttribute("error", true);
+            request.setAttribute("errorMessage", "All required fields should be filled (firstName, lastName, phone, date)");
+            System.out.println(PERSON_PATH + " " + action + " " + id);
+            if ("edit".equals(action)) {
+               setPersonToForm(request, id);
+            };
+            getServletContext().getRequestDispatcher(PERSON_FORM).forward(request, response);
+            return;
+        } else {
+            request.setAttribute("error", false);
+        }
+
         dto.setUpdated(updated);
         dto.setFirstName(firstName);
         dto.setLastName(lastName);
@@ -78,5 +112,11 @@ public class PersonController extends HttpServlet {
         }
         response.sendRedirect(PREP_ALL_PERSONS);
 
+    }
+
+    private void setPersonToForm(HttpServletRequest request, int id) {
+        PersonDto dto = PERSON_SERVICE.read(id);
+        request.setAttribute("person", dto);
+        request.setAttribute("action", "edit");
     }
 }
--- src/main/webapp/WEB-INF/jsp/formPersonDetails.jsp
+++ src/main/webapp/WEB-INF/jsp/formPersonDetails.jsp
@@ -6,10 +6,11 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Person Details</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/WEB-INF/css/style.css"/>
 </head>
 <body>
 <jsp:include page="/WEB-INF/jsp/header.jsp"/>
@@ -22,10 +23,13 @@
     Address: <input type="text" name="address" value="${person.address}"/><br/>
     Phone: <input type="text" name="phone" value="${person.phone}"/><br/>
     Notes: <input name="notes" value="${person.notes}"/><br/>
-    Year of birth: <input type="text" name="birthYear" value="${person.birthYear}"/><br/>
-    Month of birth: <input type="text" name="birthMonth" value="${person.birthMonth}"/><br/>
-    Day of birth: <input type="text" name="birthDay" value="${person.birthDay}"/><br/>
+    Year of birth (RRRR): <input type="text" name="birthYear" value="${person.birthYear}"/><br/>
+    Month of birth (MM): <input type="text" name="birthMonth" value="${person.birthMonth}"/><br/>
+    Day of birth (DD): <input type="text" name="birthDay" value="${person.birthDay}"/><br/>
     <input type="submit" value="Save"/>
+    <c:if test="${error}">
+        <span class="error">${errorMessage}</span>
+    </c:if>
 </form>
 
 <jsp:include page="/WEB-INF/jsp/footer.jsp"/>
