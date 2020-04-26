a1443aefd5b12abba5783a5a47c811864355f6b6
Paweł Dąbrowski <fluorix@gmail.com>
2020-04-26 21:05:17 +0200

    LaborMapperTest in progress

--- src/test/java/pl/coderslab/labor/LaborMapperTest.java
+++ src/test/java/pl/coderslab/labor/LaborMapperTest.java
@@ -4,16 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import pl.coderslab.commons.GenericDao;
 import pl.coderslab.commons.MapperInterface;
+import pl.coderslab.commons.ServiceInterface;
 import pl.coderslab.employee.Employee;
 import pl.coderslab.employee.EmployeeDaoImpl;
 import pl.coderslab.employee.EmployeeDto;
 import pl.coderslab.employee.EmployeeEntity;
 import pl.coderslab.employee.EmployeeMapper;
+import pl.coderslab.employee.EmployeeService;
 import pl.coderslab.vehicle.Vehicle;
 import pl.coderslab.vehicle.VehicleDaoImpl;
 import pl.coderslab.vehicle.VehicleDto;
 import pl.coderslab.vehicle.VehicleEntity;
 import pl.coderslab.vehicle.VehicleMapper;
+import pl.coderslab.vehicle.VehicleService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -31,10 +34,14 @@ public class LaborMapperTest {
     Labor labor;
     LaborDto laborDto;
     Vehicle vehicle;
+    VehicleDto vehicleDto;
     Employee employee;
+    EmployeeDto employeeDto;
     final GenericDao<EmployeeEntity> employeeDao = mock(EmployeeDaoImpl.class);
+    final ServiceInterface<EmployeeDto> employeeService = mock(EmployeeService.class);
     final MapperInterface<EmployeeDto, Employee, EmployeeEntity> employeeMapper = mock(EmployeeMapper.class);
     final GenericDao<VehicleEntity> vehicleDao = mock(VehicleDaoImpl.class);
+    final ServiceInterface<VehicleDto> vehicleService = mock(VehicleService.class);
     final MapperInterface<VehicleDto, Vehicle, VehicleEntity> vehicleMapper = mock(VehicleMapper.class);
     final MapperInterface<LaborDto, Labor, LaborEntity> laborMapper = new LaborMapper(employeeDao, employeeMapper, vehicleDao, vehicleMapper);
 
@@ -102,6 +109,113 @@ public class LaborMapperTest {
         assertEquals(laborDtoExpected.getMaterialCost(), laborDtoActual.getMaterialCost(), 0.1);
         assertEquals(laborDtoExpected.getMhTotal(), laborDtoActual.getMhTotal());
         assertEquals(laborDtoExpected.getCustomerFullname(), laborDtoActual.getCustomerFullname());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void shouldThrowNullPointerWhenMappingNullInServiceToDto() {
+
+        // given
+        labor = null;
+
+        // when
+        laborMapper.mapServiceToDto(labor);
+    }
+
+    @Test
+    public void shouldMapDtoToServiceWork() {
+
+        // given
+        laborDto = new LaborDto(
+                1,
+                LocalDate.of(1999, 1, 1),
+                LocalDate.of(1999, 1, 2),
+                LocalDate.of(1999, 1, 3),
+                LocalDate.of(1999, 1, 4),
+                1,
+                "employeeFullname",
+                "issue",
+                "service",
+                "QUEUE",
+                1,
+                "(1999) mark model registryPlace",
+                1.0,
+                1.0,
+                100,
+                "customerFullname"
+        );
+        Labor laborExpected = new Labor(
+                1,
+                LocalDate.of(1999, 1, 1),
+                LocalDate.of(1999, 1, 2),
+                LocalDate.of(1999, 1, 3),
+                LocalDate.of(1999, 1, 4),
+                employee,
+                "issue",
+                "service",
+                Labor.StatusEnum.QUEUE,
+                vehicle,
+                1.0,
+                1.0,
+                100,
+                null,
+                null,
+                false
+        );
+        when(employeeService.read(1)).thenReturn(employeeDto);
+        when(employeeMapper.mapDtoToService(employeeDto)).thenReturn(employee);
+        when(vehicleService.read(1)).thenReturn(vehicleDto);
+        when(vehicleMapper.mapDtoToService(vehicleDto)).thenReturn(vehicle);
+
+        // when
+        Labor laborActual = laborMapper.mapDtoToService(laborDto);
+
+        // then
+        assertEquals(laborExpected, laborActual);
+        assertEquals(laborExpected.getStartedDate(), laborActual.getStartedDate());
+        assertEquals(laborExpected.getFinishedDate(), laborActual.getFinishedDate());
+        assertEquals(laborExpected.getDescriptionIssue(), laborActual.getDescriptionIssue());
+        assertEquals(laborExpected.getDescriptionService(), laborActual.getDescriptionService());
+        assertEquals(laborExpected.getStatus(), laborActual.getStatus());
+        assertEquals(laborExpected.getCustomerCost(), laborActual.getCustomerCost(), 0.1);
+        assertEquals(laborExpected.getMaterialCost(), laborActual.getMaterialCost(), 0.1);
+        assertEquals(laborExpected.getMhTotal(), laborActual.getMhTotal());
+        assertNull(laborActual.getCreated());
+        assertNull(laborActual.getUpdated());
+        assertFalse(laborActual.isActive());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void shouldThrowNullPointerWhenMappingNullInDtoToService() {
+
+        // given
+        laborDto = null;
+
+        // when
+        laborMapper.mapDtoToService(laborDto);
+    }
+
+    @Test
+    public void shouldMapServiceToEntityWork() {
+
+        // given
+        labor = new Labor(
+                1,
+                LocalDate.of(1999, 1, 1),
+                LocalDate.of(1999, 1, 2),
+                LocalDate.of(1999, 1, 3),
+                LocalDate.of(1999, 1, 4),
+                employee,
+                "issue",
+                "service",
+                Labor.StatusEnum.QUEUE,
+                vehicle,
+                1.0,
+                1.0,
+                100,
+                LocalDateTime.of(2020, 12, 31, 15, 35),
+                LocalDateTime.of(2020, 12, 31, 15, 35),
+                true
+        );
 
     }
 }
